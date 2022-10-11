108544ee04ec7ad1148be054ec1e2947028f126a
Anastasia E <90329657+ananummy@users.noreply.github.com>
2022-10-12 00:31:20 +0400

    fix: add dots before img addresses

--- stuff.js
+++ stuff.js
@@ -1,41 +1,41 @@
 const headphones = [
     {
-        img: "/assets/img/Image1.png",
+        img: "./assets/img/Image1.png",
         title: "Apple BYZ S852I",
         price: 2927,
         rate: 4.7,
         id: 1.1,
     },
     {
-        img: "/assets/img/Image2.png",
+        img: "./assets/img/Image2.png",
         title: "Apple EarPods",
         price: 2327,
         rate: 4.5,
         id: 1.2,
     },
     {
-        img: "/assets/img/Image3.png",
+        img: "./assets/img/Image3.png",
         title: "Apple EarPods",
         price: 2327,
         rate: 4.5,
         id: 1.3,
     },
     {
-        img: "/assets/img/Image1.png",
+        img: "./assets/img/Image1.png",
         title: "Apple BYZ S852I",
         price: 2927,
         rate: 4.7,
         id: 1.4,
     },
     {
-        img: "/assets/img/Image2.png",
+        img: "./assets/img/Image2.png",
         title: "Apple EarPods",
         price: 2327,
         rate: 4.5,
         id: 1.5,
     },
     {
-        img: "/assets/img/Image3.png",
+        img: "./assets/img/Image3.png",
         title: "Apple EarPods",
         price: 2327,
         rate: 4.5,
@@ -45,21 +45,21 @@ const headphones = [
 
 const wirelessHeadphones = [
     {
-        img: "/assets/img/image_1.png",
+        img: "./assets/img/image_1.png",
         title: "Apple AirPods",
         price: 9527,
         rate: 4.7,
         id: 2.1,
     },
     {
-        img: "/assets/img/image_2.png",
+        img: "./assets/img/image_2.png",
         title: "GERLAX GH-04",
         price: 6527,
         rate: 4.7,
         id: 2.2,
     },
     {
-        img: "/assets/img/image_3.png",
+        img: "./assets/img/image_3.png",
         title: "BOROFONE BO4",
         price: 7527,
         rate: 4.7,
